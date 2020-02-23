0d90a018cbb2b48e8d95f2c883357dd27bd6b5f3
Ashley Allphin <ashley.allphin@gmail.com>
2020-02-23 13:12:11 -0500

    game is working, need to code reset button

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,10 @@
 // ==========================================================
 
 var choices = ["r", "p", "s"];
+var winCount = 0;
+var lossCount = 0;
+var tieCount = 0;
+
 
 
 
@@ -33,16 +37,65 @@ document.onkeyup = function() {
 
     if (playerChoice === "r" || playerChoice === "p" || playerChoice === "s") {
         
-        // testing
-        // console.log ("r, p, or s")
-
+        //if both the computer and player choose "r"
+        if (computerChoice === "r" && playerChoice === "r") {
+            tieCount++;
+            console.log("Ties: " + tieCount);
         
+        //if both the computer and player choose "p"
+        } else if (computerChoice === "p" && playerChoice === "p") {
+            tieCount++;
+            console.log("Ties: " + tieCount);
 
+        //if both the computer and player choose "s"
+        } else if (computerChoice === "s" && playerChoice === "s") {
+            tieCount++;
+            console.log("Ties: " + tieCount);
+
+        } else if (computerChoice === "r" && playerChoice === "p") {
+            winCount++;
+            console.log("Wins: " + winCount);
+        
+        } else if (computerChoice === "r" && playerChoice === "s") {
+            lossCount++;
+            console.log("Losses: " + lossCount);
+        
+        } else if (computerChoice === "p" && playerChoice === "r") {
+            lossCount++;
+            console.log("Losses: " + lossCount);
+        
+        } else if (computerChoice === "p" && playerChoice === "s") {
+            winCount++;
+            console.log("Wins: " + winCount);
+        
+        } else if (computerChoice === "s" && playerChoice === "r") {
+            winCount++;
+            console.log("Wins: " + winCount);
+        
+        } else if (computerChoice === "s" && playerChoice === "p") {
+            lossCount++;
+            console.log("Losses: " + lossCount);
+        }
     } else {
+        //if none of the situations above occur, the player must have chosen a letter other than "r," "p," or "s"
         alert ("Please select an r, p, or s.")
     }
+    
+    //show counters on page
+    document.getElementById("win-count-html").innerHTML = winCount;
+    document.getElementById("loss-count-html").innerHTML = lossCount;
+    document.getElementById("tie-count-html").innerHTML = tieCount;
 
 }
 
+
+
+
+
+
+
+
+
 // CALL FUNCTIONS
 // ==========================================================
+
