e1473bd120297ea8ed880627587adcc85d946dd9
Ashley Allphin <ashley.allphin@gmail.com>
2020-02-23 14:39:36 -0500

    game functions and keeps track of score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,8 @@
 // ==========================================================
 
 var choices = ["r", "p", "s"];
+var computerChoiceImage = ["assets/images/rock.svg", "assets/images/paper.svg", "assets/images/scissors.svg"]
+var playerChoiceImage = ["assets/images/rock.svg", "assets/images/paper.svg", "assets/images/scissors.svg"]
 var winCount = 0;
 var lossCount = 0;
 var tieCount = 0;
@@ -20,71 +22,89 @@ var tieCount = 0;
 // FUNCTIONS
 // ==========================================================
 
-document.onkeyup = function() {
-    
-    //after the key is released, create playerChoice that is the keycode that's attached to the key they pressed -- turn it into string and turn it into lowercase
-    var playerChoice = String.fromCharCode(event.keyCode).toLowerCase();
 
-    //testing
-    console.log(playerChoice);
+window.onload = function() {
+
+    document.onkeyup = function() {
+        
+        //after the key is released, create playerChoice that is the keycode that's attached to the key they pressed -- turn it into string and turn it into lowercase
+        var playerChoice = String.fromCharCode(event.keyCode).toLowerCase();
+
+        //testing
+        console.log(playerChoice);
 
 
-    //computer makes its choice
-    var computerChoice = choices[Math.floor(Math.random()*choices.length)];
+        //computer makes its choice
+        var computerChoice = choices[Math.floor(Math.random()*choices.length)];
 
-    //testing
-    console.log(computerChoice);
+        //testing
+        console.log(computerChoice);
 
-    if (playerChoice === "r" || playerChoice === "p" || playerChoice === "s") {
+        if (playerChoice === "r" || playerChoice === "p" || playerChoice === "s") {
         
-        //if both the computer and player choose "r"
-        if (computerChoice === "r" && playerChoice === "r") {
-            tieCount++;
-            console.log("Ties: " + tieCount);
-        
-        //if both the computer and player choose "p"
-        } else if (computerChoice === "p" && playerChoice === "p") {
-            tieCount++;
-            console.log("Ties: " + tieCount);
+            computerChoice
 
-        //if both the computer and player choose "s"
-        } else if (computerChoice === "s" && playerChoice === "s") {
-            tieCount++;
-            console.log("Ties: " + tieCount);
+            //if both the computer and player choose "r"
+            if (computerChoice === "r" && playerChoice === "r") {
+                tieCount++;
+            
+            //if both the computer and player choose "p"
+            } else if (computerChoice === "p" && playerChoice === "p") {
+                tieCount++;
 
-        } else if (computerChoice === "r" && playerChoice === "p") {
-            winCount++;
-            console.log("Wins: " + winCount);
-        
-        } else if (computerChoice === "r" && playerChoice === "s") {
-            lossCount++;
-            console.log("Losses: " + lossCount);
-        
-        } else if (computerChoice === "p" && playerChoice === "r") {
-            lossCount++;
-            console.log("Losses: " + lossCount);
-        
-        } else if (computerChoice === "p" && playerChoice === "s") {
-            winCount++;
-            console.log("Wins: " + winCount);
-        
-        } else if (computerChoice === "s" && playerChoice === "r") {
-            winCount++;
-            console.log("Wins: " + winCount);
-        
-        } else if (computerChoice === "s" && playerChoice === "p") {
-            lossCount++;
-            console.log("Losses: " + lossCount);
+            //if both the computer and player choose "s"
+            } else if (computerChoice === "s" && playerChoice === "s") {
+                tieCount++;
+
+            } else if (computerChoice === "r" && playerChoice === "p") {
+                winCount++;
+            
+            } else if (computerChoice === "r" && playerChoice === "s") {
+                lossCount++;
+            
+            } else if (computerChoice === "p" && playerChoice === "r") {
+                lossCount++;
+            
+            } else if (computerChoice === "p" && playerChoice === "s") {
+                winCount++;
+            
+            } else if (computerChoice === "s" && playerChoice === "r") {
+                winCount++;
+            
+            } else if (computerChoice === "s" && playerChoice === "p") {
+                lossCount++;
+            }
+        } else {
+            //if none of the situations above occur, the player must have chosen a letter other than "r," "p," or "s"
+            alert ("Please select an R, a P, or an S.")
         }
-    } else {
-        //if none of the situations above occur, the player must have chosen a letter other than "r," "p," or "s"
-        alert ("Please select an r, p, or s.")
+        
+
+        //show plays in img
+
+        // if computer plays rock, show rock image
+        if (computerChoice === "r") {
+            document.getElementById("choice-image-computer").src = "assets/images/rock.svg";
+        } else if (computerChoice === "p") {
+                document.getElementById("choice-image-computer").src = "assets/images/paper.svg";
+        } else {
+        document.getElementById("choice-image-computer").src = "assets/images/scissors.svg";
+        }
+
+        if (playerChoice === "r") {
+            document.getElementById("choice-image-player").src = "assets/images/rock.svg";
+        } else if (playerChoice === "p") {
+                document.getElementById("choice-image-player").src = "assets/images/paper.svg";
+        } else {
+        document.getElementById("choice-image-player").src = "assets/images/scissors.svg";
+        }
+
+        //show counters on page
+        document.getElementById("win-count-html").innerHTML = winCount;
+        document.getElementById("loss-count-html").innerHTML = lossCount;
+        document.getElementById("tie-count-html").innerHTML = tieCount;
+
     }
-    
-    //show counters on page
-    document.getElementById("win-count-html").innerHTML = winCount;
-    document.getElementById("loss-count-html").innerHTML = lossCount;
-    document.getElementById("tie-count-html").innerHTML = tieCount;
 
 }
 
